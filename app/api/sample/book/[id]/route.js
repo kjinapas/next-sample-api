b6609e66b7aeaf89f94b67f0b20e14ccb319eb03
kjinapas <kjinapas@hotmail.com>
2024-03-30 17:02:44 +0700

    update key

--- app/api/sample/book/[id]/route.js
+++ app/api/sample/book/[id]/route.js
@@ -3,7 +3,9 @@ import { books } from "../book_data";
 
 export async function GET(request,{params}){
     const data = books.slice(0,params.id)
-    return Response.json({"results":data})
+    return Response.json({"results":data.map((book,index)=>({
+        ...book,id:(index+1).toString()
+    }))})
 }
 
 
--- app/api/sample/book/route.js
+++ app/api/sample/book/route.js
@@ -2,7 +2,11 @@ import { books } from "./book_data";
 import { NextResponse } from 'next/server';
 
 export async function GET(){
-    return NextResponse.json({"results":books})
+
+    return NextResponse.json({"results":books.map((book,index)=>({
+        ...book,
+        id:(index+1).toString()
+    }))})
 
 }
 
